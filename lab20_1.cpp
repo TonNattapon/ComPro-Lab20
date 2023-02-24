3dd179fe3ea2271e1bba5a2632a0da344d41e3dc
= <=>
2023-02-24 12:09:18 +0700

    Complete

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect
+{
+	double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect a, Rect b)
+{
+	double x, y;
+	x = min((a.x + a.w), (b.x + b.w)) - max(a.x, b.x);
+	y = min(a.y, b.y) - max((a.y - a.h), (b.y - b.h));
 
+	if (x > 0 && y > 0)
+		return x * y;
+	else
+		return 0;
+}
 
-
+int main()
+{
+	Rect R1 = {-1, 2, 6.9, 9.6};
+	Rect R2 = {0, 0, 1.2, 2.5};
+	cout << overlap(R1, R2);
 }
